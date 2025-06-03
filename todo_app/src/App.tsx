0ee06c59a4ba8e59f6f2f8b148dfa4143f1c5fb7
mepavankumar15 <mepavankumar15@gmail.com>
2025-06-03 17:03:44 +0530

    Initial commit

---
+++ todo_app/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className='bg-purple-950 p-200 min-h-screen flex justify-center items-center '>
+      <div className='max-w-[1000px] w-[90%] bg-slate-800 p-4'></div>
+     </div>
+    </>
+  )
+}
+
+export default App
